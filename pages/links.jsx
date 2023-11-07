7067c68613b0f8b03e7a36b55a67377b68ed31f0
Binay Kumar Pradhan <bpradhan.asli@gmail.com>
2023-11-07 19:14:11 +0100

    some content

--- pages/links.jsx
+++ pages/links.jsx
@@ -1,5 +1,70 @@
-function links() {
-    return <h1>links page!</h1>
-  }
+import { Card } from 'primereact/card';
+import { ScrollTop } from 'primereact/scrolltop';
+
+function Resources() {
+    return (
+        <div className='flex flex-column py-6 justify-content-center'>
+        <Card title="Admission Procedure at Brandenburg Technical University">
+          <div style={{height: '400px', 'overflow': 'auto' }}>
+            <p className="m-0">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
+                numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+            </p>
+            <ScrollTop target="parent" threshold={100} icon="pi pi-arrow-up text-base" />
+          </div>
+        </Card>
+
+        <Card title="Finding Accomodation">
+          <div style={{height: '400px', 'overflow': 'auto' }}>
+            <p className="m-0">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
+                numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+            </p>
+            <ScrollTop target="parent" threshold={100} icon="pi pi-arrow-up text-base" />
+          </div>
+        </Card>
+
+        <Card title="City Registration">
+          <div style={{height: '400px', 'overflow': 'auto' }}>
+            <p className="m-0">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
+                numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+            </p>
+            <ScrollTop target="parent" threshold={100} icon="pi pi-arrow-up text-base" />
+          </div>
+        </Card>
+
+        <Card title="Opening Bank Account">
+          <div style={{height: '400px', 'overflow': 'auto' }}>
+            <p className="m-0">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
+                numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+            </p>
+            <ScrollTop target="parent" threshold={100} icon="pi pi-arrow-up text-base" />
+          </div>
+        </Card>
+
+        <Card title="Reaching Cottbus">
+          <div style={{height: '400px', 'overflow': 'auto' }}>
+            <p className="m-0">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
+                numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+            </p>
+            <ScrollTop target="parent" threshold={100} icon="pi pi-arrow-up text-base" />
+          </div>
+        </Card>
+        
+        <Card title="Emgergency Contacts">
+          <div style={{height: '400px', 'overflow': 'auto' }}>
+            <p className="m-0">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
+                numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+            </p>
+            <ScrollTop target="parent" threshold={100} icon="pi pi-arrow-up text-base" />
+          </div>
+        </Card>
+        </div>
+    )
+  };
   
-  export default links
+  export default Resources
