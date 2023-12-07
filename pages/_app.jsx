d118a7c7387273dd509375945dbe97ac5883dbac
Binay Kumar Pradhan <bpradhan.asli@gmail.com>
2023-12-08 00:21:20 +0100

    updated theme

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,4 @@
-import 'primereact/resources/themes/lara-dark-blue/theme.css'; // theme
+import 'primereact/resources/themes/lara-light-blue/theme.css'; // theme
 import 'primereact/resources/primereact.min.css'; // core css
 import 'primeicons/primeicons.css'; // icons
 import 'primeflex/primeflex.css';
