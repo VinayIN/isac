5fb8f74454732e1221a5e27bf92e9535441ef979
Binay Kumar Pradhan <bpradhan.asli@gmail.com>
2024-02-13 18:49:02 +0100

    gallarx design changed

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,7 +3,6 @@ import 'primereact/resources/primereact.min.css'; // core css
 import 'primeicons/primeicons.css'; // icons
 import 'primeflex/primeflex.css';
 import '../styles/global.css';
-import '../styles/gallery.css';
 import '../src/global.js';
 import Layout from '../components/layout';
 
--- pages/gallery.jsx
+++ pages/gallery.jsx
@@ -1,260 +1,159 @@
-import { useState, useEffect } from 'react';
-import React from 'react';
-import { Galleria } from 'primereact/galleria';
-import { Card } from 'primereact/card';
-import { Carousel } from 'primereact/carousel';
+import React, { useState, useEffect } from 'react';
+import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
 import { Dialog } from 'primereact/dialog';
-import Image from 'next/image';
+import { Galleria } from 'primereact/galleria';
+import { Dropdown } from 'primereact/dropdown';
+
 
 const MyGallery = () => {
+  const imagesData = [
+    {
+      title: 'Holi 2023',
+      year: '2023',
+      images: [
+        '/images/Holi/1.jpg',
+        '/images/Holi/2.jpg'
+        // Add more images for Card 1
+      ],
+    },
+    {
+      title: 'Independent Day 2023',
+      year: '2023',
+      images: [
+        '/images/independent/1.JPG',
+        '/images/independent/2.JPG',
+        '/images/independent/3.JPG',
+        '/images/independent/4.JPG',
+        '/images/independent/5.JPG',
+        '/images/independent/6.JPG',
+        '/images/independent/7.JPG',
+        '/images/independent/8.JPG',
+        '/images/independent/9.JPG',
+        '/images/independent/10.JPG',
 
-    const [inside, setInside] = useState(false);
-    const [position, setPosition] = useState('bottom');
-    const [visible, setVisible] = useState(false);
-    const [selectedImages, setSelectedImages] = useState([]);
-    const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-    const imagesData = [
-        {
-          title: 'Holi 2023',
-          images: [
-            '/images/Holi/1.jpg',
-            '/images/Holi/2.jpg'
-            // Add more images for Card 1
-          ],
-        },
-        {
-          title: 'Independent Day 2023',
-          images: [
-            '/images/independent/1.JPG',
-            '/images/independent/2.JPG',
-            '/images/independent/3.JPG',
-            '/images/independent/4.JPG',
-            '/images/independent/5.JPG',
-            '/images/independent/6.JPG',
-            '/images/independent/7.JPG',
-            '/images/independent/8.JPG',
-            '/images/independent/9.JPG',
-            '/images/independent/10.JPG',
-
-            // Add more images for Card 2
-          ]
-        },
-        {
-            title: 'Navratri 2023',
-            images: [
-              '/images/Navratri/1.JPG',
-              '/images/Navratri/2.JPG',
-              '/images/Navratri/3.JPG',
-              '/images/Navratri/4.JPG',
-              '/images/Navratri/5.JPG',
-              '/images/Navratri/6.JPG',
-              '/images/Navratri/7.JPG',
-              '/images/Navratri/8.JPG',
-              '/images/Navratri/9.JPG',
-              '/images/Navratri/10.JPG',
-              '/images/Navratri/11.JPG',
-              '/images/Navratri/12.JPG',
-              '/images/Navratri/13.JPG',
-              '/images/Navratri/14.jpg',
-              '/images/Navratri/16.jpg',
-              // Add more images for Card 2
-            ]
-          },
-          {
-            title: 'Senftenberg Beach',
-            images: [
-              '/images/Senftenberg Beach/1.jpg',
-              '/images/Senftenberg Beach/2.jpeg',
-              '/images/Senftenberg Beach/3.jpeg',
-              '/images/Senftenberg Beach/4.jpg',
-              '/images/Senftenberg Beach/5.JPG',
-              '/images/Senftenberg Beach/6.JPG',
-              '/images/Senftenberg Beach/7.JPG',
-              '/images/Senftenberg Beach/8.JPG',
-              '/images/Senftenberg Beach/9.JPG',
-              '/images/Senftenberg Beach/10.JPG',
-              '/images/Senftenberg Beach/11.JPG',
-              '/images/Senftenberg Beach/12.JPG',
-              // Add more images for Card 2
-            ]
-          }
-        // Add more cards with their images
-      ];
-    
-
-  const images = [
-    {
-        previewImageSrc: '/images/Senftenberg Beach/1.jpg',
-        thumbnailImageSrc: '/images/Senftenberg Beach/1.jpg',
-        alt: 'Senftenberg Beach',
-        title: 'Senftenberg Beach'
+        // Add more images for Card 2
+      ]
     },
     {
-        previewImageSrc: '/images/Navratri/1.JPG',
-        thumbnailImageSrc: '/images/Navratri/1.JPG',
-        alt: 'Navratri',
-        title: 'Navratri'
+        title: 'Navratri 2023',
+        year: '2023',
+        images: [
+          '/images/Navratri/1.JPG',
+          '/images/Navratri/2.JPG',
+          '/images/Navratri/3.JPG',
+          '/images/Navratri/4.JPG',
+          '/images/Navratri/5.JPG',
+          '/images/Navratri/6.JPG',
+          '/images/Navratri/7.JPG',
+          '/images/Navratri/8.JPG',
+          '/images/Navratri/9.JPG',
+          '/images/Navratri/10.JPG',
+          '/images/Navratri/11.JPG',
+          '/images/Navratri/12.JPG',
+          '/images/Navratri/13.JPG',
+          '/images/Navratri/14.jpg',
+          '/images/Navratri/16.jpg',
+          // Add more images for Card 2
+        ]
     },
     {
-        previewImageSrc: '/images/independent/1.JPG',
-        thumbnailImageSrc: '/images/independent/1.JPG',
-        alt: 'independent',
-        title: 'independent'
-    },
-    {
-        previewImageSrc: '/images/Holi/1.jpg',
-        thumbnailImageSrc: '/images/Holi/1.jpg',
-        alt: 'Holi',
-        title: 'Holi'
-    },
-    {
-        previewImageSrc: '/images/independent/2.JPG',
-        thumbnailImageSrc: '/images/independent/2.JPG',
-        alt: 'independent',
-        title: 'independent'
-    },
-    {
-      previewImageSrc: '/images/Navratri/3.JPG',
-      thumbnailImageSrc: '/images/Navratri/3.JPG',
-      alt: 'Navratri',
-      title: 'Navratri'
-    },
-    {
-      previewImageSrc: '/images/Holi/2.jpg',
-      thumbnailImageSrc: '/images/Holi/2.jpg',
-      alt: 'Holi',
-      title: 'Holi'
-    },
-    {
-      previewImageSrc: '/images/Navratri/10.JPG',
-      thumbnailImageSrc: '/images/Navratri/10.JPG',
-      alt: 'Navratri',
-      title: 'Navratri'
-    },
-    {
-      previewImageSrc: '/images/Senftenberg Beach/4.jpg',
-      thumbnailImageSrc: '/images/Senftenberg Beach/4.jpg',
-      alt: 'Senftenberg Beach',
-      title: 'Senftenberg Beach'
-    },
-    {
-      previewImageSrc: '/images/Holi/2.jpg',
-      thumbnailImageSrc: '/images/Holi/2.jpg',
-      alt: 'Holi',
-      title: 'Holi'
-    },
-    {
-      previewImageSrc: '/images/Senftenberg Beach/10.JPG',
-      thumbnailImageSrc: '/images/Senftenberg Beach/10.JPG',
-      alt: 'Senftenberg Beach',
-      title: 'Senftenberg Beach'
-    },
-    {
-      previewImageSrc: '/images/Navratri/16.jpg',
-      thumbnailImageSrc: '/images/Navratri/16.jpg',
-      alt: 'Navratri',
-      title: 'Navratri'
-    },
-    {
-      previewImageSrc: '/images/Navratri/7.JPG',
-      thumbnailImageSrc: '/images/Navratri/7.JPG',
-      alt: 'Navratri',
-      title: 'Navratri'
-    },
-    {
-      previewImageSrc: '/images/Navratri/4.JPG',
-      thumbnailImageSrc: '/images/Navratri/4.JPG',
-      alt: 'Navratri',
-      title: 'Navratri'
-    },
-    {
-      previewImageSrc: '/images/Senftenberg Beach/8.JPG',
-      thumbnailImageSrc: '/images/Senftenberg Beach/8.JPG',
-      alt: 'Senftenberg Beach',
-      title: 'Senftenberg Beach'
+      title: 'Senftenberg Beach',
+      year: '2022',
+      images: [
+        '/images/Senftenberg Beach/1.jpg',
+        '/images/Senftenberg Beach/2.jpeg',
+        '/images/Senftenberg Beach/3.jpeg',
+        '/images/Senftenberg Beach/4.jpg',
+        '/images/Senftenberg Beach/5.JPG',
+        '/images/Senftenberg Beach/6.JPG',
+        '/images/Senftenberg Beach/7.JPG',
+        '/images/Senftenberg Beach/8.JPG',
+        '/images/Senftenberg Beach/9.JPG',
+        '/images/Senftenberg Beach/10.JPG',
+        '/images/Senftenberg Beach/11.JPG',
+        '/images/Senftenberg Beach/12.JPG',
+        // Add more images for Card 2
+      ]
     }
-    
-    // ... more images
+    // Add more cards with their images
   ];
+  
+  const [layout, setLayout] = useState('list');
+  const [selectedImageData, setSelectedImageData] = useState(null);
+  const [displayDialog, setDisplayDialog] = useState(false);
+  const [selectedYear, setSelectedYear] = useState('All');
 
-  const responsiveOptions = [
-    {
-      breakpoint: '1024px',
-      numVisible: 3
-    },
-    {
-      breakpoint: '768px',
-      numVisible: 2
-    },
-    {
-      breakpoint: '560px',
-      numVisible: 1
-    }
-  ];
+  useEffect(() => {
+    const years = imagesData.map(item => item.year);
+    const uniqueYears = ['All', ...new Set(years)].sort().reverse();
+    setSelectedYear(uniqueYears.includes(selectedYear) ? selectedYear : uniqueYears[0]);
+  }, [imagesData, selectedYear]);
 
-  const itemTemplate = (item) => {
-    return <img src={item.previewImageSrc} alt={item.alt} style={{ width: '80%', height: 'calc(100vh - 100px)' }} />;
+  const filteredData = selectedYear === 'All' ? imagesData : imagesData.filter(item => item.year === selectedYear);
+
+  const getRandomThumbnail = (images) => {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    return images[randomIndex];
   };
 
-  const showImagesInCarousel = (images) => {
-    setSelectedImages(images);
-    setCurrentImageIndex(0);
-    setVisible(true);
+  const gridLayout = (data) => (
+    <div className="col-12 sm:col-6 lg:col-3 p-2 m-auto shadow-2" onClick={() => {
+      setSelectedImageData(data);
+      setDisplayDialog(true);
+    }}>
+      <div className="card">
+        <img src={getRandomThumbnail(data.images)} alt={data.title} style={{ width: 'auto', height: '180px', objectFit: 'cover'}} />
+        <h4 style={{ textAlign: 'center' }}>{data.title}</h4>
+      </div>
+    </div>
+  );
+
+  const listLayout = (data) => (
+    <div className="col-12 m-auto shadow-2" onClick={() => {
+      setSelectedImageData(data);
+      setDisplayDialog(true);
+    }}>
+      <div className="card" style={{ display: 'flex', alignItems: 'center', padding: '1rem' }}>
+        <img src={getRandomThumbnail(data.images)} alt={data.title} style={{ width: '80px', height: '120px', objectFit: 'cover' }} />
+        <h4>{data.title}</h4>
+      </div>
+    </div>
+  );
+
+  const itemTemplate = (data, layoutMode) => {
+    return layoutMode === 'list' ? listLayout(data) : gridLayout(data);
   };
 
-  const hideImage = () => {
-    setVisible(false);
-  };
+  const renderGalleriaDialog = () => (
+    <Dialog header={selectedImageData?.title} visible={displayDialog} onHide={() => setDisplayDialog(false)} style={{ width: '50vw' }} modal>
+      <Galleria value={selectedImageData?.images} item={(item) => <img src={item} alt={selectedImageData?.title} style={{ width: '100%' }} />} showThumbnails autoPlay transitionInterval={3000} />
+    </Dialog>
+  );
 
-  const cards = imagesData.map((card, index) => (
-    <Card
-      key={index}
-      title={card.title}
-      style={{ cursor: 'pointer' }}
-      onClick={() => showImagesInCarousel(card.images)}
-      className="custom-card"
-    >
-      <img src={card.images[0]} alt={card.title} style={{ width: '100%' }} />
-    </Card>
-  ));
+  const renderDropdown = () => {
+    const years = ['All', ...new Set(imagesData.map(item => item.year))].sort();
+    return (
+      <Dropdown 
+        value={selectedYear} 
+        options={years.map(year => ({label: year, value: year}))} 
+        onChange={(e) => setSelectedYear(e.value)} 
+        style={{marginRight: '8px', minWidth: '120px'}}
+        placeholder="Select a Year"
+      />
+    );
+  };
 
   return (
-
-    <>
-    <Galleria value={images} circular autoPlay transitionInterval={1000} responsiveOptions={responsiveOptions} style={{ width: '100%', height: 'calc(100vh - 100px)' }} showThumbnails={false} showIndicators
-          showIndicatorsOnItem={inside} indicatorsPosition={position} item={itemTemplate} />
-          <div style={{ paddingTop: '6%' }}>
-
-              <div className="gallery_card grid-container">{cards}
-
-              <Dialog
-  visible={visible}
-  onHide={hideImage}
-  style={{ width: '70%', maxHeight: '100vh' }} // Set a maximum height
-  className="dialog-without-scrollbar"
->
-  <Galleria
-    value={selectedImages}
-    circular
-    autoPlay
-    transitionInterval={1500}
-    responsiveOptions={responsiveOptions}
-    style={{ width: '100%' }}
-    showThumbnails={false}
-    showIndicators
-    showIndicatorsOnItem={inside}
-    indicatorsPosition={position}
-    item={(imageSrc) => (
-      <img src={imageSrc} alt={`Image ${currentImageIndex + 1}`} style={{ width: '80%', height: '70vh' , objectFit: 'contain'}} />
-    )}
-  />
-</Dialog>
-
-              </div>
-              </div>
-          </>
-        
+    <div>
+      <DataView value={filteredData} layout={layout} itemTemplate={itemTemplate}
+      header= {
+      <div className="flex m-auto">
+        {renderDropdown()}
+        <DataViewLayoutOptions layout={layout} onChange={(e) => setLayout(e.value)} />
+      </div>}
+       paginator rows={5}/>
+      {selectedImageData && renderGalleriaDialog()}
+    </div>
   );
 };
 
