83ab4f6c491b3266ea543e3ee2599cc67ab2a1a0
Binay Kumar Pradhan <bpradhan.asli@gmail.com>
2024-02-13 15:09:44 +0100

    markdown files added

--- pages/links.jsx
+++ pages/links.jsx
@@ -1,71 +1,54 @@
+import React from 'react';
 import { Card } from 'primereact/card';
-import { ScrollTop } from 'primereact/scrolltop';
 import { Button } from 'primereact/button';
+import { ScrollTop } from 'primereact/scrolltop';
+import ReactMarkdown from 'react-markdown';
+import { useMarkdown } from '../components/usemarkdown';
 
 function Links() {
-    return (
-        <div className='flex flex-column py-6'>
-        <Card title="Admission Procedure at Brandenburg Technical University" className='m-2'>
-          <div style={{height: '400px', 'overflow': 'auto' }}>
+  const admissionContent = useMarkdown('/markdown/admissionProcedure.md');
+  const accommodationContent = useMarkdown('/markdown/findingAccommodation.md');
+  const registrationContent = useMarkdown('/markdown/cityRegistration.md');
+  const accountContent = useMarkdown('/markdown/bankAccount.md');
+  const cottbusContent = useMarkdown('/markdown/reachingCottbus.md');
+  const contactsContent = useMarkdown('/markdown/emergencyContacts.md');
+
+  return (
+    <div className='flex flex-column py-6'>
+      <Card title="Admission Procedure at Brandenburg Technical University" className='m-2 shadow-4'>
+        <div style={{height: '500px', overflow: 'auto' }}>
+          <ReactMarkdown>{admissionContent}</ReactMarkdown>
+        </div>
+        <ScrollTop target="parent" threshold={100} icon="pi pi-arrow-up text-base" />
+
+        <Card title="Check the official link of BTU" subTitle="Here, we provide some basic information which could be a help for your decision-making process." className="flex justify-content-center shadow-4">
+          <div>
             <p className="m-0">
-              Please note that the admission procedures for international students at Brandenburg University of Technology Cottbus-Senftenberg (BTU Cottbus-Senftenberg) may change over time, and it&apos;s essential to check the official university website or contact the university&apos;s admissions office for the most up-to-date information.
-              Here&apos;s a general overview of the admission procedure for international students at BTU for both the summer and winter intakes:
+              <a href="https://www.b-tu.de/en/prospective-international-students" className="no-underline px-2">
+                <Button icon="pi pi-arrow-right" label="BTU official page" iconPos="right"/>
+              </a>
+              <a href="https://www.b-tu.de/en/students/admissions-registrars-office/dates-and-deadlines" className="no-underline px-2">
+                <Button icon="pi pi-arrow-right" label="Check deadlines here" iconPos="right"/>
+              </a>
+              <a href="https://www.b-tu.de/en/international/news/events" className="no-underline px-2" >
+                <Button icon="pi pi-arrow-right" label="International Events" iconPos="right"/>
+              </a>
             </p>
-            <p>
-              Summer Intake (Starting in April) & Winter Intake (Starting in October)
-            </p>
-            <ul>
-              <li>
-                  Choose a Program: 
-                  Ensure that you meet the program&apos;s admission requirements, including ECTS requirements and language proficiency.
-              </li>
-              <li>
-                  Check Application Deadlines:
-                  Every degree program has a different deadline. Pay attention to the perspective international student&apos;s deadline.
-              </li>
-              <li>
-                  Online Application:
-                  If you are planning to have a scholarship, start with your scholarship application at least 2 months before the admission portal opens.
-                  Make sure to provide all required documents, including your academic transcripts, proof of language proficiency (usually IELTS or TOEFL), and a copy of your passport.
-                  <p>
-                    There are 2 possible ways to apply for your application:
-                    <ol>
-                      <li><a href="https://www.uni-assist.de/en/how-to-apply">Uni-Assist</a></li>
-                      <li><a href="https://www.b-tu.de/mybtu">BTU Portal</a></li>
-                    </ol>
-                    Check where you should apply!
-                  </p>
-              </li>
-              <li>
-                  Admission Evaluation:
-                  The university&apos;s admissions committee will review your application and documents. If you meet the requirements, you will receive an offer of admission. Upon which you can then proceed to search for accommodation.
-              </li>
-            </ul>
-            <p>
-              If you accept the offer of admission and follow the instructions provided for obtaining a student visa, You will reach Cottbus in no time.
+          </div>
+        </Card>
+
+      </Card>
+
+      <Card title="Finding Accomodation" className='m-2 shadow-4'>
+          <div style={{height: '500px', 'overflow': 'auto' }}>
+            <p className="m-0">
+            Under Construction!
             </p>
             <ScrollTop target="parent" threshold={100} icon="pi pi-arrow-up text-base" />
           </div>
-          
-          <Card title="Check the official link of BTU" subTitle="Here, we provide some basic information which could be a help for your decision-making process." className="flex justify-content-center">
-            <div>
-              <p className="m-0">
-                <a href="https://www.b-tu.de/en/prospective-international-students" className="no-underline px-2">
-                  <Button icon="pi pi-arrow-right" label="BTU official page" iconPos="right"/>
-                </a>
-                <a href="https://www.b-tu.de/en/students/admissions-registrars-office/dates-and-deadlines" className="no-underline px-2">
-                  <Button icon="pi pi-arrow-right" label="Check deadlines here" iconPos="right"/>
-                </a>
-                <a href="https://www.b-tu.de/en/international/news/events" className="no-underline px-2" >
-                  <Button icon="pi pi-arrow-right" label="International Events" iconPos="right"/>
-                </a>
-              </p>
-            </div>
-          </Card>
-
         </Card>
 
-        <Card title="Finding Accomodation" className='m-2'>
+        <Card title="City Registration" className='m-2 shadow-4'>
           <div style={{height: '400px', 'overflow': 'auto' }}>
             <p className="m-0">
             Under Construction!
@@ -74,17 +57,8 @@ function Links() {
           </div>
         </Card>
 
-        <Card title="City Registration" className='m-2'>
-          <div style={{height: '400px', 'overflow': 'auto' }}>
-            <p className="m-0">
-            Under Construction!
-            </p>
-            <ScrollTop target="parent" threshold={100} icon="pi pi-arrow-up text-base" />
-          </div>
-        </Card>
-
-        <Card title="Opening Bank Account" className='m-2'>
-          <div style={{height: '400px', 'overflow': 'auto' }}>
+        <Card title="Opening Bank Account" className='m-2 shadow-4'>
+          <div style={{height: '500px', 'overflow': 'auto' }}>
             <p className="m-0">
                 Under Construction!
             </p>
@@ -92,8 +66,8 @@ function Links() {
           </div>
         </Card>
 
-        <Card title="Reaching Cottbus" className='m-2'>
-          <div style={{height: '400px', 'overflow': 'auto' }}>
+        <Card title="Reaching Cottbus" className='m-2 shadow-4'>
+          <div style={{height: '500px', 'overflow': 'auto' }}>
             <p className="m-0">
             Under Construction!
             </p>
@@ -101,16 +75,16 @@ function Links() {
           </div>
         </Card>
         
-        <Card title="Emgergency Contacts" className='m-2'>
-          <div style={{height: '400px', 'overflow': 'auto' }}>
+        <Card title="Emgergency Contacts" className='m-2 shadow-4'>
+          <div style={{height: '500px', 'overflow': 'auto' }}>
             <p className="m-0">
             Under Construction!
             </p>
             <ScrollTop target="parent" threshold={100} icon="pi pi-arrow-up text-base" />
           </div>
         </Card>
-        </div>
-    )
-  };
-  
-  export default Links
+    </div>
+  );
+};
+
+export default Links;
---
+++ public/markdown/admissionProcedure.md
@@ -0,0 +1,17 @@
+Please note that the admission procedures for international students at Brandenburg University of Technology Cottbus-Senftenberg `(BTU Cottbus-Senftenberg)` may change over time, and it's essential to check the official university website or contact the university's admissions office for the most up-to-date information. Here's a general overview of the admission procedure for international students at BTU for both the summer and winter intakes:
+
+Summer Intake (Starting in April) & Winter Intake (Starting in October)
+
+- **Choose a Program:** Ensure that you meet the program's admission requirements, including ECTS requirements and language proficiency.
+- **Check Application Deadlines:** Every degree program has a different deadline. Pay attention to the perspective international student's deadline.
+- **Online Application:** If you are planning to have a scholarship, start with your scholarship application at least 2 months before the admission portal opens. Make sure to provide all required documents, including your academic transcripts, proof of language proficiency (usually IELTS or TOEFL), and a copy of your passport.
+  
+  There are 2 possible ways to apply for your application:
+  1. [Uni-Assist](https://www.uni-assist.de/en/how-to-apply)
+  2. [BTU Portal](https://www.b-tu.de/mybtu)
+  
+  Check where you should apply!
+  
+- **Admission Evaluation:** The university's admissions committee will review your application and documents. If you meet the requirements, you will receive an offer of admission. Upon which you can then proceed to search for accommodation.
+
+If you accept the offer of admission and follow the instructions provided for obtaining a student visa, You will reach Cottbus in no time.
