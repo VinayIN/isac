52bab0ab6141b143575fb3b441b4f0a5c155cd44
Binay Kumar Pradhan <bpradhan.asli@gmail.com>
2024-04-11 15:59:43 +0200

    gallery added from firestore

--- pages/gallery.jsx
+++ pages/gallery.jsx
@@ -1,169 +1,79 @@
-import React, { useState, useEffect } from 'react';
-import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
+import React, { useState, useEffect, useMemo } from 'react';
+import { DataView } from 'primereact/dataview';
 import { Dialog } from 'primereact/dialog';
 import { Galleria } from 'primereact/galleria';
-import { Dropdown } from 'primereact/dropdown';
-import Image from 'next/image';
-
+import { useFirestore } from '../hooks/firestore';
+import { Card } from 'primereact/card';
 
 const MyGallery = () => {
-  const imagesData = [
-    {
-      title: 'Holi 2023',
-      year: '2023',
-      images: [
-        '/images/Holi/1.jpg',
-        '/images/Holi/2.jpg'
-        // Add more images for Card 1
-      ],
-    },
-    {
-      title: 'Independent Day 2023',
-      year: '2023',
-      images: [
-        '/images/independent/1.JPG',
-        '/images/independent/2.JPG',
-        '/images/independent/3.JPG',
-        '/images/independent/4.JPG',
-        '/images/independent/5.JPG',
-        '/images/independent/6.JPG',
-        '/images/independent/7.JPG',
-        '/images/independent/8.JPG',
-        '/images/independent/9.JPG',
-        '/images/independent/10.JPG',
+  const galleryData = useFirestore('gallery');
+  const [galleryState, setGalleryState] = useState({
+    years: [],
+    selectedYear: null,
+    selectedData: null,
+    displayDialog: false
+  });
 
-        // Add more images for Card 2
-      ]
-    },
-    {
-        title: 'Navratri 2023',
-        year: '2023',
-        images: [
-          '/images/Navratri/1.JPG',
-          '/images/Navratri/2.JPG',
-          '/images/Navratri/3.JPG',
-          '/images/Navratri/4.JPG',
-          '/images/Navratri/5.JPG',
-          '/images/Navratri/6.JPG',
-          '/images/Navratri/7.JPG',
-          '/images/Navratri/8.JPG',
-          '/images/Navratri/9.JPG',
-          '/images/Navratri/10.JPG',
-          '/images/Navratri/11.JPG',
-          '/images/Navratri/12.JPG',
-          '/images/Navratri/13.JPG',
-          '/images/Navratri/14.jpg',
-          '/images/Navratri/16.jpg',
-          // Add more images for Card 2
-        ]
-    },
-    {
-      title: 'Senftenberg Beach',
-      year: '2022',
-      images: [
-        '/images/Senftenberg Beach/1.jpg',
-        '/images/Senftenberg Beach/2.jpeg',
-        '/images/Senftenberg Beach/3.jpeg',
-        '/images/Senftenberg Beach/4.jpg',
-        '/images/Senftenberg Beach/5.JPG',
-        '/images/Senftenberg Beach/6.JPG',
-        '/images/Senftenberg Beach/7.JPG',
-        '/images/Senftenberg Beach/8.JPG',
-        '/images/Senftenberg Beach/9.JPG',
-        '/images/Senftenberg Beach/10.JPG',
-        '/images/Senftenberg Beach/11.JPG',
-        '/images/Senftenberg Beach/12.JPG',
-        // Add more images for Card 2
-      ]
-    }
-    // Add more cards with their images
-  ];
-  
-  const [layout, setLayout] = useState('list');
-  const [selectedImageData, setSelectedImageData] = useState(null);
-  const [displayDialog, setDisplayDialog] = useState(false);
-  const [selectedYear, setSelectedYear] = useState('All');
+  // Memoize groupedData
+  const groupedData = useMemo(() => {
+    if (!galleryData.data) return {};
+    return galleryData.data.reduce((acc, item) => {
+      const year = item.year;
+      acc[year] = acc[year] || [];
+      acc[year].push(item);
+      return acc;
+    }, {});
+  }, [galleryData.data]);
 
   useEffect(() => {
-    const years = imagesData.map(item => item.year);
-    const uniqueYears = ['All', ...new Set(years)].sort().reverse();
-    setSelectedYear(uniqueYears.includes(selectedYear) ? selectedYear : uniqueYears[0]);
-  }, [imagesData, selectedYear]);
+    const uniqueYears = Object.keys(groupedData).sort().reverse();
+    setGalleryState(prevState => ({
+      ...prevState,
+      years: uniqueYears
+    }));
+  }, [groupedData]);
 
-  const filteredData = selectedYear === 'All' ? imagesData : imagesData.filter(item => item.year === selectedYear);
-
-  const getRandomThumbnail = (images) => {
-    const randomIndex = Math.floor(Math.random() * images.length);
-    return images[randomIndex];
+  const handleYearClick = (year) => {
+    setGalleryState(prevState => ({
+      ...prevState,
+      selectedYear: year,
+      selectedData: groupedData[year],
+      displayDialog: true
+    }));
   };
 
-  const gridLayout = (data) => (
-    <div className="col-12 sm:col-6 lg:col-3 p-2 m-auto shadow-2" onClick={() => {
-      setSelectedImageData(data);
-      setDisplayDialog(true);
-    }}>
-      <div className="card mb-1" style={{ textAlign: 'center' }}>
-        <img src={getRandomThumbnail(data.images)} alt={data.title} style={{ width: 'auto', height: '180px', objectFit: 'cover'}} />
-        <h4 style={{ textAlign: 'center' }}>{data.title}</h4>
-      </div>
+  const yearItemTemplate = (year) => (
+    <div className="year-card clickable m-4" key={year} onClick={() => handleYearClick(year)}>
+        {year}
     </div>
   );
-
-  const listLayout = (data) => (
-    <div className="col-12 m-auto shadow-2" onClick={() => {
-      setSelectedImageData(data);
-      setDisplayDialog(true);
-    }}>
-      <div className="card" style={{ display: 'flex', padding: '1rem' }}>
-        <img src={getRandomThumbnail(data.images)} alt={data.title} style={{ width: '80px', height: '120px', objectFit: 'cover' }} />
-        <h4>{data.title}</h4>
-      </div>
-    </div>
-  );
-
-  const itemTemplate = (data, layoutMode) => {
-    return layoutMode === 'list' ? listLayout(data) : gridLayout(data);
-  };
-
-  const renderGalleriaDialog = () => (
-    <Dialog header={selectedImageData?.title} visible={displayDialog} onHide={() => setDisplayDialog(false)} style={{ width: '65vw' }} modal>
-      <Galleria value={selectedImageData?.images}
-                item={(item) => (
-                  <div style={{ width: '100%', height: 'auto', position: 'relative' }}>
-                    <Image src={item} alt={selectedImageData?.title} width={900} height={800} quality={70} style={{ width: '100%', height: 'auto' }}/>
-                    <a href={item} download={`${selectedImageData?.title}.jpg`}>
-                      <button className="p-button p-button-primary p-button-text">Download</button>
-                    </a>
-                  </div>
-                )}
-                circular showIndicators showItemNavigators showThumbnails={false}>
-      </Galleria>
-    </Dialog>
-  );  
-
-  const renderDropdown = () => {
-    const years = ['All', ...new Set(imagesData.map(item => item.year))].sort();
-    return (
-      <Dropdown 
-        value={selectedYear} 
-        options={years.map(year => ({label: year, value: year}))} 
-        onChange={(e) => setSelectedYear(e.value)} 
-        style={{marginRight: '8px', minWidth: '120px'}}
-        placeholder="Select a Year"
-      />
-    );
-  };
 
   return (
-    <div className='mx-4'>
-      <DataView value={filteredData} layout={layout} itemTemplate={itemTemplate}
-      header= {
-      <div className="flex m-auto">
-        {renderDropdown()}
-        <DataViewLayoutOptions layout={layout} onChange={(e) => setLayout(e.value)} />
-      </div>}
-       paginator rows={5}/>
-      {selectedImageData && renderGalleriaDialog()}
+    <div className="m-4">
+        <h3>Year-Wise pictures from the events are categorized here: </h3>
+        <DataView
+            value={galleryState.years}
+            layout="list"
+            itemTemplate={yearItemTemplate}
+        />
+      <Dialog header={galleryState.selectedYear} visible={galleryState.displayDialog} onHide={() => setGalleryState(prevState => ({ ...prevState, displayDialog: false }))} style={{ width: '65vw' }} modal>
+        <Galleria value={galleryState.selectedData}
+          item={(item) => (
+            <div style={{ width: '100%', height: 'auto', position: 'relative' }}>
+              <Card title={item.name}>
+                All the images are shared here.
+                <a href={item.link1} target="_blank" rel="noreferrer">
+                (Link)
+                </a>
+              </Card>
+            </div>
+          )}
+          circular
+          showIndicators
+          showItemNavigators
+          showThumbnails={false}
+        />
+      </Dialog>
     </div>
   );
 };
